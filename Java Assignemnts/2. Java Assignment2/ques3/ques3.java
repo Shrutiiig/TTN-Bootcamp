29e1ab975d89f3aa0b843a8b197e78d860a4866a
Shruti <gshruti4567@gmail.com>
2021-02-19 10:58:07 +0530

    Updating Java8 Assignment

--- Java Assignemnts/2. Java Assignment2/ques3/ques3.java
+++
@@ -1,8 +0,0 @@
-// WAP to produce NoClassDefFoundError and ClassNotFoundException exceptions.
-
-class ques{
-    public static void main(String[] args) {
-
-        System.out.println("ClassNotFoundException not there");
-    }
-}
--- Java Assignemnts/2. Java Assignment2/ques3/ques3a.java
+++
@@ -1,9 +0,0 @@
-public class ques3a {
-    public static void main(String[] args) {
-        try{
-            Class.forName("RandomClass");
-        }catch(Exception e){
-            System.out.println(e);
-        }
-    }
-}
